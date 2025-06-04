dda62fa414b789b14272ef4acadaf71e9bd2c5e9
Mostafa <memo605.work@gmail.com>
2025-06-04 17:41:43 +0300

    Create 4 functions for: adding, subtracting, multiplying, and dividing.

---
+++ main.js
@@ -0,0 +1,30 @@
+const add = function(a,b) {
+    let result = a + b;
+    return result
+    };
+    
+    
+const subtract = function(a,b) {
+ let result = a - b;
+ return result
+ };
+
+ const multiply = function(a,b) {
+    let result = a * b;
+    return result
+    };
+
+
+const divide = function(a,b) {
+let result = a / b;
+return result
+};
+
+
+let num1 = 0;
+let num2 = 0;
+let operator="";
+
+function operate(num1,num2,operator) {
+    
+}
