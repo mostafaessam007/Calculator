b3f4e6d5c6cc5da4cb7ce7587acdd26c46b545e0
Mostafa <memo605.work@gmail.com>
2025-07-02 08:35:46 +0300

    Add calcualtor main function.

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+let screen= document.querySelector(".screen")
+let remove= document.querySelector("#delete");
+let clear= document.querySelector("#clear");
+let dot = document.querySelector("#dot");
+let equal = document.querySelector("#equal")
+let numButtons = document.querySelectorAll(".num-btn")
+let operators = document.querySelectorAll(".ope")
 const add = function(a,b) {
     let result = a + b;
     return result
@@ -20,11 +27,75 @@ let result = a / b;
 return result
 };
 
-
-let num1 = 0;
-let num2 = 0;
+let currentNumber ="";
+let num1 = "";
+let num2 = "";
 let operator="";
+let numbers = "";
 
-function operate(num1,num2,operator) {
+// function operate(num1,operator, num2) {
+// add(a,b)
+// }
+
+clear.addEventListener('click', ()=> {
+    screen.textContent= numbers = "";
+});
+
+remove.addEventListener('click', ()=> {
+    screen.textContent= numbers=numbers.substring(0,numbers.length-1);
+});
+
+numButtons.forEach(btn => {
+    btn.addEventListener('click',(e)=>{
+        numbers +=  e.target.textContent
+        if (numbers.length <=9) {
+                    screen.textContent=numbers;
+
+        }
+    })
     
+});
+operators.forEach(btn => {
+    btn.addEventListener('click',(e)=>{
+        if (numbers === "") return;
+        if (numbers.length <=9) {
+        num1=numbers;
+        operator =  e.target.textContent;
+        numbers+= operator;
+        screen.textContent=numbers;
+        }
+    })
+    
+});
+
+equal.addEventListener("click",()=>{
+     calculate()
+} );
+
+function calculate(){
+
+    const parts = numbers.split(/(\+|\-|\*|\/)/);
+    num1=Number(parts[0]);
+    operator = parts[1]
+    num2=Number(parts[2]);
+
+    let result;
+
+    if (operator === "+") {
+        result = add (num1,num2);
+    } else if (operator === "-"){
+        result = subtract (num1,num2);
+    }
+    else if (operator === "*"){
+        result = multiply (num1,num2);
+    }else if (operator === "/"){
+        if (num2 === 0) {
+            result = "ERROR!"
+        }else {
+          result = divide (num1,num2);  
+        } 
+    }
+    numbers = result.toString();
+    screen.textContent = numbers;
+
 }
